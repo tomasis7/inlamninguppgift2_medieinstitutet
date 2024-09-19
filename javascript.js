d1eaa2eb0450cc26f5b92ed0b63ae14689041a29
johanv-mi <johan.victorin@medieinstitutet.se>
2024-09-19 11:58:51 +0200

    added a searchbar

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function openSearch() {
+  document.getElementById("loppensearchbar").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("loppensearchbar").style.display = "none";
+}
