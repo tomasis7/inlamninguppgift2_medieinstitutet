839af3651d7bf297f6ef18b923747bed126d8b89
tomasis7 <tomasis7@gmail.com>
2024-09-19 12:57:20 +0200

    not perfect .. at least a start. I forgot to merge from java branch

--- javascript.js
+++ javascript.js
@@ -5,3 +5,18 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("loppensearchbar").style.display = "none";
 }
+
+var coll = document.getElementsByClassName("menu");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var nav = this.nextElementSibling;
+    if (nav.style.maxHeight) {
+      nav.style.maxHeight = null;
+    } else {
+      nav.style.maxHeight = nav.scrollHeight + "px";
+    }
+  });
+}
